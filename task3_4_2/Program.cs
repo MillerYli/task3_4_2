37256d692267df5573027ec7c10cdacf1d32cb2a
Yulia Miller <miller82.98@mail.ru>
2022-12-05 13:12:00 -0800

    Добавьте файлы проекта.

---
+++ task3_4_2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Name
+{
+    class Programm
+    {
+        public static void Main(string[] args)
+        {
+            DaysOfWeek MyFaforiteDay;
+
+            MyFaforiteDay = DaysOfWeek.Friday;
+
+            Console.WriteLine(MyFaforiteDay);
+
+        }
+    }
+    enum DaysOfWeek : byte
+    {
+        Monday = 1,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
+        Saturday,
+        Sunday
+
+    }
+}
+
